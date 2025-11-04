892317f07c243d6a0dd8568ace7ade2f4b7110b8
AHNAF ALIF TSABITUL 'AZMI DHIYAUL HAQ <ahnafalf05@gmail.com>
2025-11-04 09:23:50 +0700

    push

--- ArrayBilangan1.java
+++ ArrayBilangan1.java
@@ -1,10 +1,10 @@
 public class ArrayBilangan1 {
     public static void main(String[] args) {
-        // Inisialisasi array langsung saat deklarasi
+        
         int[] bil = {5, 13, -7, 17};
 
-        // Menampilkan isi array menggunakan perulangan
-        for (int i = 0; i < 4; i++) {
+        
+        for (int i = 0; i <= 4; i++) {
             System.out.println(bil[i]);
         }
     }
