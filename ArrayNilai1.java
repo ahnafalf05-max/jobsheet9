9849a8e2af2c3bc6544512c31c8a3ff515c7941a
AHNAF ALIF TSABITUL 'AZMI DHIYAUL HAQ <ahnafalf05@gmail.com>
2025-11-04 19:25:37 +0700

    modify

--- ArrayNilai1.java
+++ ArrayNilai1.java
@@ -1,20 +1,23 @@
+
 import java.util.Scanner;
+
 
 public class ArrayNilai1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
-        int[] nilaiAkhir = new int[10];
+        int[] finalScore = new int[10];
 
         
         for (int i = 0; i < 10; i++) {
-            System.out.print("Masukkan nilai akhir ke-" + i + " : ");
-            nilaiAkhir[i] = sc.nextInt();
+            System.out.print("Enter the final score " + i + " : ");
+            finalScore[i] = sc.nextInt();
         }
         for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+            System.out.println("Final score " + i + " is " + finalScore[i]);
+            
         }
 
-        sc.close();
+        
     }
 }
