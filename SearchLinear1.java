e764e410a7fd9a5a75ca76c905e4c0f37ae8d025
AHNAF ALIF TSABITUL 'AZMI DHIYAUL HAQ <ahnafalf05@gmail.com>
2025-11-04 20:00:21 +0700

    modify

---
+++ SearchLinear1.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class SearchLinear1 {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter the number of array elements: ");
+            int n = input.nextInt();
+            int[] arrayInt = new int[n];
+            
+            
+            for (int i = 0; i < n; i++) {
+                System.out.print("Enter the array element " + i + ": ");
+                arrayInt[i] = input.nextInt();
+            }
+            
+            
+            System.out.print("Enter the key you want to search for: ");
+            int key = input.nextInt();
+            
+            
+            int result = -1;
+            for (int i = 0; i < arrayInt.length; i++) {
+                if (arrayInt[i] == key) {
+                    result = i;
+                    break;
+                }
+            }
+            
+            
+            if (result != -1) {
+                System.out.println("The key in the array is located at index position " + result);
+            } else {
+                System.out.println("Key not found in the array.");
+            }
+        }
+    }
+}
+/* 
+1. Explain the meaning of the break; statement on line 13 of the program code in 
+Experiment 4. 
+is used inside a for loop to immediately exit the loop once the key is found
+2. Modify the program code in experiment 4 so that the program can receive input 
+in the form of the number of array elements, the contents of the array, and the 
+key you want to search for. Then, print to the screen the index of the element 
+positions of the searched key.
+Done
+3.
+
+
+
+
+
+
+
+*/
