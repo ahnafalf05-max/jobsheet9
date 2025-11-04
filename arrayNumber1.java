3461747378163548b2e884b974a975aafa19ba8f
AHNAF ALIF TSABITUL 'AZMI DHIYAUL HAQ <ahnafalf05@gmail.com>
2025-11-04 19:11:20 +0700

    error

--- arrayNumber1.java
+++ arrayNumber1.java
@@ -2,18 +2,24 @@ public class arrayNumber1 {
     public static void main(String[] args) {
         
         int[] num = new int[4];
-        num[0] = 5;
-        num[1] = 12;
-        num[2] = 7;
-        num[3] = 20;
+        num[0] = 5.0;
+        num[1] = 12867;
+        num[2] = 7.5;
+        num[3] = 200000;
     System.out.println(num[0]);
     System.out.println(num[1]);
     System.out.println(num[2]);
-    System.out.println(num[3]);
-    
-
-        
-        
+    System.out.println(num[3]);   
     }
 }
 
+/* 
+1.If the contents of each element of the array num are changed with numbers 5.0, 12867, 
+7.5, 2000000. What happens? How can it be like that?  
+int[] means every element must be an integer
+5.0 and 7.5 are floatingpoint numbers not integers
+
+
+
+
+*/
